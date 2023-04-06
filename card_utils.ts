25cb8945630d6d959c37f9ebb673e5f5e3fee5cc
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2023-04-06 23:28:35 +0200

    Unified classes

--- card_utils.ts
+++ card_utils.ts
@@ -1,4 +1,4 @@
-import { Carta, CartaColorataNonNumerata, CartaColorataNumerata, CartaSpeciale } from "./classes";
+import { Carta } from "./classes";
 
 
 export function generateDeck() {
@@ -6,67 +6,67 @@ export function generateDeck() {
     //generate cards
     //19 carte, da 0 a 9, di colore blu
     for (let i = 0; i <= 9; i++) {
-        cards.push(new CartaColorataNumerata(i, "blue", `cards_images/b${i}.png`))
+        cards.push(new Carta("standard", "blue", `cards_images/b${i}.png`, i))
     }
     for (let i = 1; i <= 9; i++) {
-        cards.push(new CartaColorataNumerata(i, "blue", `cards_images/b${i}.png`))
+        cards.push(new Carta("standard", "blue", `cards_images/b${i}.png`, i))
     }
-    cards.push(new CartaColorataNonNumerata("plustwo", "blue", `cards_images/bplustwo.png`))
-    cards.push(new CartaColorataNonNumerata("plustwo", "blue", `cards_images/bplustwo.png`))
-    cards.push(new CartaColorataNonNumerata("stop", "blue", `cards_images/bstop.png`))
-    cards.push(new CartaColorataNonNumerata("stop", "blue", `cards_images/bstop.png`))
-    cards.push(new CartaColorataNonNumerata("reverse", "blue", `cards_images/breverse.png`))
-    cards.push(new CartaColorataNonNumerata("reverse", "blue", `cards_images/breverse.png`))
+    cards.push(new Carta("plustwo", "blue", `cards_images/bplustwo.png`, -1))
+    cards.push(new Carta("plustwo", "blue", `cards_images/bplustwo.png`, -1))
+    cards.push(new Carta("stop", "blue", `cards_images/bstop.png`, -1))
+    cards.push(new Carta("stop", "blue", `cards_images/bstop.png`, -1))
+    cards.push(new Carta("reverse", "blue", `cards_images/breverse.png`, -1))
+    cards.push(new Carta("reverse", "blue", `cards_images/breverse.png`, -1))
 
     //19 carte, da 0 a 9, di colore verde
     for (let i = 0; i <= 9; i++) {
-        cards.push(new CartaColorataNumerata(i, "green", `cards_images/g${i}.png`))
+        cards.push(new Carta("standard", "green", `cards_images/g${i}.png`, i))
     }
     for (let i = 1; i <= 9; i++) {
-        cards.push(new CartaColorataNumerata(i, "green", `cards_images/g${i}.png`))
+        cards.push(new Carta("standard", "green", `cards_images/g${i}.png`, i))
     }
-    cards.push(new CartaColorataNonNumerata("plustwo", "green", `cards_images/gplustwo.png`))
-    cards.push(new CartaColorataNonNumerata("plustwo", "green", `cards_images/gplustwo.png`))
-    cards.push(new CartaColorataNonNumerata("stop", "green", `cards_images/gstop.png`))
-    cards.push(new CartaColorataNonNumerata("stop", "green", `cards_images/gstop.png`))
-    cards.push(new CartaColorataNonNumerata("reverse", "green", `cards_images/greverse.png`))
-    cards.push(new CartaColorataNonNumerata("reverse", "green", `cards_images/greverse.png`))
+    cards.push(new Carta("plustwo", "green", `cards_images/gplustwo.png`, -1))
+    cards.push(new Carta("plustwo", "green", `cards_images/gplustwo.png`, -1))
+    cards.push(new Carta("stop", "green", `cards_images/gstop.png`,-1))
+    cards.push(new Carta("stop", "green", `cards_images/gstop.png`, -1))
+    cards.push(new Carta("reverse", "green", `cards_images/greverse.png`, -1))
+    cards.push(new Carta("reverse", "green", `cards_images/greverse.png`, -1))
 
     //19 carte, da 0 a 9, di colore rosso
     for (let i = 0; i <= 9; i++) {
-        cards.push(new CartaColorataNumerata(i, "red", `cards_images/r${i}.png`))
+        cards.push(new Carta("standard", "red", `cards_images/r${i}.png`, i))
     }
     for (let i = 1; i <= 9; i++) {
-        cards.push(new CartaColorataNumerata(i, "red", `cards_images/r${i}.png`))
+        cards.push(new Carta("standard", "red", `cards_images/r${i}.png`, i))
     }
-    cards.push(new CartaColorataNonNumerata("plustwo", "red", `cards_images/rplustwo.png`))
-    cards.push(new CartaColorataNonNumerata("plustwo", "red", `cards_images/rplustwo.png`))
-    cards.push(new CartaColorataNonNumerata("stop", "red", `cards_images/rstop.png`))
-    cards.push(new CartaColorataNonNumerata("stop", "red", `cards_images/rstop.png`))
-    cards.push(new CartaColorataNonNumerata("reverse", "red", `cards_images/rreverse.png`))
-    cards.push(new CartaColorataNonNumerata("reverse", "red", `cards_images/rreverse.png`))
+    cards.push(new Carta("plustwo", "red", `cards_images/rplustwo.png`, -1))
+    cards.push(new Carta("plustwo", "red", `cards_images/rplustwo.png`, -1))
+    cards.push(new Carta("stop", "red", `cards_images/rstop.png`, -1))
+    cards.push(new Carta("stop", "red", `cards_images/rstop.png`, -1))
+    cards.push(new Carta("reverse", "red", `cards_images/rreverse.png`, -1))
+    cards.push(new Carta("reverse", "red", `cards_images/rreverse.png`, -1))
 
     //19 carte, da 0 a 9, di colore giallo
     for (let i = 0; i <= 9; i++) {
-        cards.push(new CartaColorataNumerata(i, "yellow", `cards_images/y${i}.png`))
+        cards.push(new Carta("standard", "yellow", `cards_images/y${i}.png`, i))
     }
     for (let i = 1; i <= 9; i++) {
-        cards.push(new CartaColorataNumerata(i, "yellow", `cards_images/y${i}.png`))
+        cards.push(new Carta("standard", "yellow", `cards_images/y${i}.png`, i))
     }
-    cards.push(new CartaColorataNonNumerata("plustwo", "yellow", `cards_images/yplustwo.png`))
-    cards.push(new CartaColorataNonNumerata("plustwo", "yellow", `cards_images/yplustwo.png`))
-    cards.push(new CartaColorataNonNumerata("stop", "yellow", `cards_images/ystop.png`))
-    cards.push(new CartaColorataNonNumerata("stop", "yellow", `cards_images/ystop.png`))
-    cards.push(new CartaColorataNonNumerata("reverse", "yellow", `cards_images/yreverse.png`))
-    cards.push(new CartaColorataNonNumerata("reverse", "yellow", `cards_images/yreverse.png`))
+    cards.push(new Carta("plustwo", "yellow", `cards_images/yplustwo.png`, -1))
+    cards.push(new Carta("plustwo", "yellow", `cards_images/yplustwo.png`, -1))
+    cards.push(new Carta("stop", "yellow", `cards_images/ystop.png`, -1))
+    cards.push(new Carta("stop", "yellow", `cards_images/ystop.png`, -1))
+    cards.push(new Carta("reverse", "yellow", `cards_images/yreverse.png`, -1))
+    cards.push(new Carta("reverse", "yellow", `cards_images/yreverse.png`, -1))
 
     //4 carte Jolly
     for (let i = 0; i < 4; i++) {
-        cards.push(new CartaSpeciale("chgcolor", `cards_images/chgcolor.png`))
+        cards.push(new Carta("chgcolor", "null", `cards_images/chgcolor.png`, -1))
     }
     //4 carte Jolly +4
     for (let i = 0; i < 4; i++) {
-        cards.push(new CartaSpeciale("plus4", `cards_images/plus4.png`))
+        cards.push(new Carta("plus4", "null", `cards_images/plus4.png`, -1))
     }
     return cards;
 }
