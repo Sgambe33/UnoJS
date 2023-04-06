f860210938301cdb42cccb80c65c8dfcf6d1e95d
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2023-04-06 16:13:57 +0200

    Handling of user connection and disconnection

--- index.ts
+++ index.ts
@@ -1,52 +1,166 @@
 import { generateDeck, shuffleDeck } from './card_utils';
-import { CartaColorataNumerata, Carta, CartaColorataNonNumerata, CartaSpeciale } from './classes';
+import {Carta, GameRoom} from './classes';
+import {v4 as uuidv4} from 'uuid';
+import * as WebSocket from 'ws';
+
+
+const cors= require('cors');
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-//create express server
-
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, `public`)));
 app.use("/cards_images", express.static(path.join(__dirname, `cards_images`)))
 
-app.listen(3000, function() {
-    console.log('Listening on port 3000');
+var module_files = fs.readdirSync(path.resolve(__dirname, `routes`), {
+  withFileTypes: true,
+});
+module_files.forEach((file) => {
+  if (!file.name.endsWith(`.ts`)) {
+    return;
+  }
+  let handler_file = require(path.join(__dirname, `routes`, file.name).replace(`.ts`, ``));
+  let route = handler_file.base_route;
+  console.log(handler_file)
+  let handler = handler_file.handler();
+
+  app.use(`/api${route}`, cors(), handler);
+});
+
+server.listen(3000, function() {
+  console.log('Listening on : http://localhost:3000');
 });
 
 
+/////////////////////////////////////////////////////////////////////////////////
 
-//create websocket server
-import * as WebSocket from 'ws';
-const server = new WebSocket.Server({ port: 8080 });
-
-
-let sockets: WebSocket[] = [];
+export let gameRooms: GameRoom[] = [];
 
 let deck: Carta[] = generateDeck();
 deck = shuffleDeck(deck);
 
-server.on('connection', function(socket: WebSocket) {
+//Use socket.io
+io.on('connection', (socket: any) => {
 
-  sockets.push(socket);
-  console.log('Connected: ' + sockets.length + ' sockets(s) connected');
-  deck = shuffleDeck(deck);
+  socket.on("message", (msg: any) => {
 
-  for(let i = 0; i < sockets.length; i++){
-    //send file to client
-    for( let j = 0; j < 7; j++){
-      
-      sockets[i].send(JSON.stringify(deck[j]));
+    let decodedMsg = JSON.parse(msg);
+
+    switch (decodedMsg.method) {
+        case "joinRoom":
+          if(gameRooms.some((room) => room.id == decodedMsg.roomId)) {
+            socket.join(decodedMsg.roomId);
+            console.log("A user joined room: " + decodedMsg.roomId);
+            let index = gameRooms.findIndex((room) => room.id == decodedMsg.roomId);
+            if (index != -1) {
+              gameRooms[index].players.push(socket.id);
+            }
+            console.log(io.sockets.adapter.rooms.get(decodedMsg.roomId));
+          } else {
+            console.log("A user tried to join an unexisting room: " + decodedMsg.roomId);
+            socket.disconnect();
+          }
+          break;
+      default:
+        console.log(decodedMsg);
     }
-  }
-
-  // When you receive a message, send that message to every socket.
-  socket.on('message', function(msg: String) {
-    sockets.forEach(s => s.send(msg));
   });
 
-  // When a socket closes, or disconnects, remove it from the array.
-  socket.on('close', function() {
-    sockets = sockets.filter(s => s !== socket);
+  socket.on("disconnect", () => {
+    //Search the user in every room
+    gameRooms.forEach((room) => {
+
+        if(room.players.includes(socket.id)){
+            room.players.splice(room.players.indexOf(socket.id), 1);
+            console.log("A user disconnected from room "+room.id);
+            if(room.players.length == 0){
+              gameRooms.splice(gameRooms.indexOf(room), 1);
+              console.log("Empty room deleted (room "+room.id+")");
+            }
+            return;
+        }
+    });
   });
+
 });
+let sockets = new Map<string, WebSocket>();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//server.on('connection', function(socket: WebSocket) {
+//  let playedCards: Carta[] = [];
+//  let playersCards = new Map<string, Carta[]>();
+//  let uuid = uuidv4();
+//
+//  sockets.set(uuid, socket);
+//  playersCards.set(uuid, []);
+//
+//  console.log('Connected: ' + sockets.size + ' sockets(s) connected');
+//  deck = shuffleDeck(deck);
+//
+//  let cards = deck.splice(0, 7);
+//  let payload = {
+//    "method": "setPlayerCards",
+//    "cards": cards
+//  }
+//  playersCards.set(uuid, cards);
+//
+//  sockets.get(uuid).send(JSON.stringify(payload));
+//
+//  // When you receive a message, send that message to eve1\y socket.
+//  socket.on('message', function(msg: string) {
+//    console.log('Message Received: ' + msg);
+//    let decodedMsg = JSON.parse(msg);
+//    switch (decodedMsg.method) {
+//        case "playCard":
+//          break;
+//        case "drawCard":
+//          break;
+//      case "joinRoom":
+//        gameRooms[0].players.push(uuid);
+//        sockets.get(uuid).send(JSON.stringify({"Success": true}));
+//        break;
+//      default:
+//        console.log(decodedMsg);
+//        break;
+//    }
+//  });
+//
+//  // When a socket closes, or disconnects, remove it from the array.
+//  socket.on('close', function() {
+//    sockets.values()
+//
+//  });
+//});
