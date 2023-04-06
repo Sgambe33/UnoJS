e097075bda0646d426951417498da20affec4109
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2023-04-06 23:29:43 +0200

    Function to extract first card

--- routes/newGameRoom.ts
+++ routes/newGameRoom.ts
@@ -3,7 +3,15 @@ import {GameRoom} from "../classes";
 const express = require(`express`);
 import { gameRooms } from "../index";
 import { v4 as uuidv4 } from "uuid";
-
+function estraiPrimaCarta(gameroom: GameRoom) {
+    let newCard = gameroom.deck.shift();
+    while (newCard.tipo != "standard") {
+        console.log("Estratta una carta non standard: " + newCard);
+        gameroom.deck.push(newCard);
+        newCard = gameroom.deck.shift();
+    }
+    return newCard;
+}
 module.exports = {
     base_route: `/newgameroom`,
     handler: () => {
@@ -13,7 +21,9 @@ module.exports = {
 
             let newUuid = uuidv4();
             console.log("Creating new gameroom requested by client with ID: "+newUuid);
-            gameRooms.push(new GameRoom(newUuid, [], "waiting"));
+            let newGameRoom = new GameRoom(newUuid, [], "waiting");
+            newGameRoom.playedCards.push(estraiPrimaCarta(newGameRoom));
+            gameRooms.push(newGameRoom);
             res.status(200).json({roomId: newUuid});
         });
         return route;
